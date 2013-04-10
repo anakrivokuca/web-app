8e87fcc470d66c4052b2809851846adc180db2b9
anakrivokuca <anakrivokuca@gmail.com>
2013-04-10 15:15:59 +0200

    Add ring support
    Add wrap-request-logging, wrap-reload and wrap-stacktrace functions.

--- project.clj
+++ project.clj
@@ -1,6 +1,5 @@
 (defproject web-app "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :description "My Clojure web application"
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [ring "1.1.8"]]
+  :main web-app.core )
--- src/web_app/core.clj
+++ src/web_app/core.clj
@@ -1,6 +1,18 @@
 (ns web-app.core)
+(use '[ring.adapter.jetty :only (run-jetty)])
+(use 'ring.middleware.reload)
+(use 'ring.middleware.stacktrace) 
+(use 'web-app.middleware)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn handler
+[{:keys [uri query-string]}]
+{:body (format "You requested %s with query %s" uri query-string)})
+
+(def app
+  (-> #'handler
+    (wrap-request-logging)
+    (wrap-reload)
+    (wrap-stacktrace)))
+
+
+(def server (run-jetty #'app {:port 8080 :join? false}))
